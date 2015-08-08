34d2511c59fd19f1a55d4e0af0be1dfe627b623e
Manuel Iurissevich <manuel.iurissevich@hotmail.it>
2015-08-08 08:14:30 +0200

    Aggiunti esperimenti e riorganizzati i file (senza aggiustare i riferimenti)

--- svmxor/kerkernel.m
+++
@@ -1,9 +0,0 @@
-function [ k ] = kerkernel( X, x )
-%KERKERNEL Summary of this function goes here
-%   Detailed explanation goes here
-
-    for i = 1:size(X,1)
-        k(i,:) = kernel(X(i,:),x);
-    end
-end
-
--- svmxor/kernel.m
+++
@@ -1,9 +0,0 @@
-function [ k ] = kernel( x1, x2 )
-%KERNEL Summary of this function goes here
-%   Detailed explanation goes here
-
-gamma = 1;
-k = (1+dot(x1,x2))^2;
-% k = gamma*norm(x2-x1)^2;
-end
-
