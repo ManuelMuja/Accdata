be47ca52f38558d630d2b88fe45993f62c2926ee
Manuel Iurissevich <manuel.iurissevich@hotmail.it>
2015-08-11 13:42:58 +0200

    commit

--- sorgente/prove_con_daniele/MAIN.m
+++ sorgente/prove_con_daniele/MAIN.m
@@ -23,12 +23,39 @@ n = 4;
 acc(:,:,n) = filter(b, a, lanciotappo(primo(n):ultimo(n),:,n));
 vel(:,:,n) = cumsum(acc(:,:,n));
 pos(:,:,n) = cumsum(vel(:,:,n));
+
 figure(3);
-  plotyy(t(primo(n):ultimo(n)), acc(:,:,n),...
-  t(primo(n):ultimo(n)), vel(:,:,n));
-print -depsc ../02guardare/vel4.eps
+subplot(221); plot(acc(:,1,n), acc(:,3,n)); ylabel('acc_z');
+subplot(222); plot(acc(:,1,n), acc(:,2,n)); xlabel('acc_y');
+subplot(223); plot(acc(:,2,n), acc(:,3,n)); xlabel('acc_x'); ylabel('acc_y');
+subplot(224); plot3(acc(:,1,n), acc(:,2,n), acc(:,3,n));
+xlabel('acc_x'); ylabel('acc_y'); zlabel('acc_z');
+drawnow;
+print -depsc ../02guardare/acc3d
+clf;
+
+
+subplot(221); plot(vel(:,1,n), vel(:,3,n)); ylabel('vel_z');
+subplot(222); plot(vel(:,1,n), vel(:,2,n)); xlabel('vel_y');
+subplot(223); plot(vel(:,2,n), vel(:,3,n)); xlabel('vel_x'); ylabel('vel_y');
+subplot(224); plot3(vel(:,1,n), vel(:,2,n), vel(:,3,n));
+xlabel('vel_x'); ylabel('vel_y'); zlabel('vel_z');
+drawnow;
+print -depsc ../02guardare/vel3d
+clf;
+
+
+subplot(221); plot(pos(:,1,n), pos(:,3,n)); ylabel('pos_z');
+subplot(222); plot(pos(:,1,n), pos(:,2,n)); xlabel('pos_y');
+subplot(223); plot(pos(:,2,n), pos(:,3,n)); xlabel('pos_x'); ylabel('pos_y');
+subplot(224); plot3(pos(:,1,n), pos(:,2,n), pos(:,3,n));
+xlabel('pos_x'); ylabel('pos_y'); zlabel('pos_z');
+drawnow;
+print -depsc ../02guardare/pos3d
+clf;
 
 
 
 % cd 01eseguire
 disegnetti;
+
